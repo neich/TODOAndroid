f0b51dbce859bfa77e8afca03fa2b5e99b22a7de
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-13 22:44:49 +0100

    New activity (before it was a fragment)

---
+++ app/src/main/java/org/udg/pds/todoandroid/activity/Login.java
@@ -0,0 +1,77 @@
+package org.udg.pds.todoandroid.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.udg.pds.todoandroid.R;
+import org.udg.pds.todoandroid.TodoApp;
+import org.udg.pds.todoandroid.entity.User;
+import org.udg.pds.todoandroid.entity.UserLogin;
+import org.udg.pds.todoandroid.rest.TodoApi;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: imartin
+ * Date: 28/03/13
+ * Time: 16:01
+ * To change this template use File | Settings | File Templates.
+ */
+
+// This is the Login fragment where the user enters the username and password and
+// then a RESTResponder_RF is called to check the authentication
+public class Login extends Activity {
+
+    TodoApi mTodoService;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mTodoService = ((TodoApp)this.getApplication()).getAPI();
+
+        Button b = (Button)findViewById(R.id.login_button);
+        // This is teh listener that will be used when the user presses the "Login" button
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText u = (EditText) Login.this.findViewById(R.id.login_username);
+                EditText p = (EditText) Login.this.findViewById(R.id.login_password);
+                Login.this.checkCredentials(u.getText().toString(), p.getText().toString());
+            }
+        });
+
+    }
+    // This method is called when the "Login" button is pressed in the Login fragment
+    public void checkCredentials(String username, String password) {
+        UserLogin ul = new UserLogin();
+        ul.username = username;
+        ul.password = password;
+        Call<User> call = mTodoService.login(ul);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+                if (response.isSuccessful()) {
+                    Login.this.startActivity(new Intent(Login.this, NavigationActivity.class));
+                } else {
+                    Toast toast = Toast.makeText(Login.this, "Error logging in", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast toast = Toast.makeText(Login.this, "Error logging in", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+}
