bad01549ddf9fb7e8d760c62fca8a5a6fc75b919
Ignacio Martín <ignacio.martin@udg.edu>
2018-03-21 21:06:17 +0100

    Added fragment

---
+++ app/src/main/java/org/udg/pds/todoandroid/fragment/FavoritesFragment.java
@@ -0,0 +1,43 @@
+package org.udg.pds.todoandroid.fragment;
+
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.Button;
+import org.udg.pds.todoandroid.R;
+import org.udg.pds.todoandroid.activity.NDActivity;
+import org.udg.pds.todoandroid.activity.NavigationActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FavoritesFragment extends Fragment {
+
+
+  public FavoritesFragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View root = inflater.inflate(R.layout.content_favorites, container, false);
+    Button button = (Button)root.findViewById(R.id.button);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent i = new Intent(FavoritesFragment.this.getActivity(), NDActivity.class);
+        startActivity(i);
+      }
+    });
+    return root;
+  }
+
+}
