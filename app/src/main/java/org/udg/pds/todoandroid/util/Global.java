3aac79156945bb53f051217217633f266dc28e6b
Ignacio Martín <natx.mc@gmail.com>
2019-01-18 15:57:39 +0100

    Use several URLs

--- app/src/main/java/org/udg/pds/todoandroid/util/Global.java
+++ app/src/main/java/org/udg/pds/todoandroid/util/Global.java
@@ -22,16 +22,17 @@ public class Global {
     // Resquest codes for startActivityForResult
     public static final int RQ_ADD_TASK = 1;
 
-    // IMPORTANT: you have to change the value of BASE_URL when deploying the app
+    // IMPORTANT: you have to change the value of BASE_URL_PORTFORWARDING when deploying the app
     // OPENSHIFT
-    // public static final String BASE_URL = "https://project2-pdsudg.rhcloud.com";
+    // public static final String BASE_URL_PORTFORWARDING = "https://project2-pdsudg.rhcloud.com";
     // Android emulator
-    // public static final String BASE_URL = "http://10.0.2.2:8080";
+    // public static final String BASE_URL_PORTFORWARDING = "http://10.0.2.2:8080";
     // Genymotion emulator
-    // public static final String BASE_URL = "http://10.0.3.2:8080";
+    // public static final String BASE_URL_PORTFORWARDING = "http://10.0.3.2:8080";
     // For debugging with real device using port forwarding
     // https://developer.chrome.com/devtools/docs/remote-debugging?hl=de#port-forwarding
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL_PORTFORWARDING = "http://localhost:8080";
+    public static final String BASE_URL_GENYMOTION = "http://10.0.3.2:8080";
 
 
 }
