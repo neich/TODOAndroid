d3bf7657dbc4e6e7092509f08c1d6031ddcfe11b
Ignacio Martín <natx.mc@gmail.com>
2020-03-02 16:03:47 +0100

    Add JsonIdentityInfo to allow serialization with ids

--- app/src/main/java/org/udg/pds/todoandroid/entity/Task.java
+++ app/src/main/java/org/udg/pds/todoandroid/entity/Task.java
@@ -1,10 +1,16 @@
 package org.udg.pds.todoandroid.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import java.util.Date;
 
 /**
  * Created by imartin on 12/02/16.
  */
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class,
+    property="id", scope = Task.class)
+
 public class Task {
   public Long id;
   public String text;
--- app/src/main/java/org/udg/pds/todoandroid/entity/User.java
+++ app/src/main/java/org/udg/pds/todoandroid/entity/User.java
@@ -1,10 +1,15 @@
 package org.udg.pds.todoandroid.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 /**
  * Created by imartin on 12/02/16.
  */
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class,
+    property="id", scope = User.class)
 public class User {
-  public long id;
+  public Long id;
   public String username;
   public String email;
 }
