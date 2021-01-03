a2cc70907df49b4177043865ede3cb028bc7bdc7
Ignacio Martín <natx.mc@gmail.com>
2021-01-03 14:31:30 +0100

    Use Navigation to go to Activity

--- app/src/main/java/org/udg/pds/todoandroid/fragment/FavoritesFragment.java
+++ app/src/main/java/org/udg/pds/todoandroid/fragment/FavoritesFragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import org.udg.pds.todoandroid.R;
 import org.udg.pds.todoandroid.activity.NavDrawerActivity;
@@ -33,8 +35,8 @@ public class FavoritesFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(FavoritesFragment.this.getActivity(), NavDrawerActivity.class);
-                startActivity(i);
+                NavDirections action = FavoritesFragmentDirections.actionActionHomeToNavDrawerActivity();
+                Navigation.findNavController(view).navigate(action);
             }
         });
         return root;
