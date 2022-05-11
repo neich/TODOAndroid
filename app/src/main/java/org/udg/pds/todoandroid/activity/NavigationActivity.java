4f9740138635924b164084dd3318adb8a088c8b2
Ignacio Martín <natx.mc@gmail.com>
2022-05-11 14:45:04 +0200

    Added Toolbar, and new method for navigation

--- app/src/main/java/org/udg/pds/todoandroid/activity/NavigationActivity.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/NavigationActivity.java
@@ -1,37 +1,70 @@
 package org.udg.pds.todoandroid.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.udg.pds.todoandroid.R;
+import org.udg.pds.todoandroid.databinding.MainBinding;
 import org.udg.pds.todoandroid.fragment.TaskList;
 
-// FragmentActivity is a base class for activities that want to use the support-based Fragment and Loader APIs.
-// http://developer.android.com/reference/android/support/v4/app/FragmentActivity.html
+// This is the main activity that contains the bottom navigation
+// This class SHOULD NOT BE CHANGED except for very specific features
 public class NavigationActivity extends AppCompatActivity {
 
-    private TaskList mTaskList;
-
-    private BottomNavigationView bottomNavigationView;
+    MainBinding binding;
+    NavHostFragment navHostFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        binding = MainBinding.inflate(this.getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbarMain);
         setUpNavigation();
     }
 
     public void setUpNavigation(){
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
+        navHostFragment = (NavHostFragment)getSupportFragmentManager()
             .findFragmentById(R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView,
+        NavigationUI.setupWithNavController(binding.bottomNavigation,
             navHostFragment.getNavController());
+        NavigationUI.setupWithNavController(binding.toolbarMain,
+            navHostFragment.getNavController());
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.settingsFragment) {
+            // NavigationUI.onNavDestinationSelected(item, NavHostFragment.findNavController(navHostFragment));
+            NavHostFragment.findNavController(navHostFragment).navigate(R.id.settingsFragment);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void navigateTo(int id) {
+        NavHostFragment.findNavController(navHostFragment).navigate(R.id.settingsFragment);
+    }
 }
