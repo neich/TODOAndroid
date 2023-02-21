467057143e60446747b66d0a307435b03c5f4ae8
Ignacio Martín <natx.mc@gmail.com>
2023-02-21 11:04:46 +0100

    New version of the class diagram

---
+++ app/src/main/java/org/udg/pds/todoandroid/TestActivity.java
@@ -0,0 +1,14 @@
+package org.udg.pds.todoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
