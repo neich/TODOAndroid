91b137d7262d7628ec6baa832e27dbf9822174b6
Ignacio Martin <natx.mc@gmail.com>
2024-03-14 20:10:40 +0100

    Added support to capture item selection on the drawer

--- app/src/main/java/org/udg/pds/todoandroid/activity/NavigationActivity.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/NavigationActivity.java
@@ -3,13 +3,20 @@ package org.udg.pds.todoandroid.activity;
 import static androidx.navigation.ui.AppBarConfigurationKt.AppBarConfiguration;
 import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,10 +31,11 @@ import org.udg.pds.todoandroid.fragment.TaskList;
 
 // This is the main activity that contains the bottom navigation
 // This class SHOULD NOT BE CHANGED except for very specific features
-public class NavigationActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     MainBinding binding;
     NavHostFragment navHostFragment;
+    DrawerLayout drawerLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,13 +49,13 @@ public class NavigationActivity extends AppCompatActivity {
         navHostFragment = (NavHostFragment)getSupportFragmentManager()
             .findFragmentById(R.id.nav_host_fragment);
         var navController = navHostFragment.getNavController();
-        DrawerLayout dl = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
         var appBarConfiguration =
             new AppBarConfiguration.Builder(
                 R.id.action_home,
                 R.id.action_image,
                 R.id.action_tasks)
-                .setOpenableLayout(dl)
+                .setOpenableLayout(drawerLayout)
                 .build();
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -55,32 +63,24 @@ public class NavigationActivity extends AppCompatActivity {
         NavigationView navView = findViewById(R.id.nav_view);
         NavigationUI.setupWithNavController(navView, navController);
 
+        navView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int selectedItemId = item.getItemId();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if(selectedItemId == R.id.action_settings) {
+            startActivity(new Intent(NavigationActivity.this, NavDrawerActivity.class));
+        }
+        else {
+            navigateTo(selectedItemId);
+        }
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.settingsFragment) {
-            // NavigationUI.onNavDestinationSelected(item, NavHostFragment.findNavController(navHostFragment));
-            NavHostFragment.findNavController(navHostFragment).navigate(R.id.settingsFragment);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void navigateTo(int id) {
-        NavHostFragment.findNavController(navHostFragment).navigate(R.id.settingsFragment);
+        NavHostFragment.findNavController(navHostFragment).navigate(id);
     }
 }
