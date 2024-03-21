2fe739f0f01b95f320665cfc860c0a0bff77186f
Ignacio Martin <natx.mc@gmail.com>
2024-03-21 12:29:31 +0100

    New activity to show information of a Task

---
+++ app/src/main/java/org/udg/pds/todoandroid/activity/TaskView.java
@@ -0,0 +1,50 @@
+package org.udg.pds.todoandroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import org.udg.pds.todoandroid.R;
+import org.udg.pds.todoandroid.databinding.ActivityAboutBinding;
+import org.udg.pds.todoandroid.databinding.ActivityTaskViewBinding;
+import org.udg.pds.todoandroid.entity.Task;
+
+public class TaskView extends AppCompatActivity {
+
+    ActivityTaskViewBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTaskViewBinding.inflate(getLayoutInflater());
+
+
+
+        var args = TaskViewArgs.fromBundle(getIntent().getExtras());
+        Task t = args.getTaskItem();
+
+        binding.tvDatecreated.setText(t.dateCreated);
+        binding.tvDatelimit.setText(t.dateLimit);
+        binding.tvText.setText(t.text);
+
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) // Press Back Icon
+        {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
