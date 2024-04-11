2c9bc895cae2bf1e8db81861a6affef2518fc08e
Ignacio Martin <natx.mc@gmail.com>
2024-04-11 18:45:18 +0200

    Implemented modern way to deal with permissions, and to send notifications at a specified time, even if the app is not in the foreground

--- app/src/main/java/org/udg/pds/todoandroid/fragment/HomeFragment.java
+++ app/src/main/java/org/udg/pds/todoandroid/fragment/HomeFragment.java
@@ -2,22 +2,34 @@ package org.udg.pds.todoandroid.fragment;
 
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
 import org.udg.pds.todoandroid.R;
+import org.udg.pds.todoandroid.TodoApp;
 import org.udg.pds.todoandroid.activity.NavigationActivity;
 import org.udg.pds.todoandroid.databinding.HomeFragmentBinding;
+import org.udg.pds.todoandroid.service.ShowNotification;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +37,20 @@ import org.udg.pds.todoandroid.databinding.HomeFragmentBinding;
 public class HomeFragment extends Fragment {
 
     private HomeFragmentBinding binding;
+
+    private TodoApp todoApp;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+        registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                HomeFragment.this.programNotification();
+            } else {
+                Toast.makeText(HomeFragment.this.requireActivity(), "You need permissions to send notifications!!!!.",
+                    Toast.LENGTH_LONG).show();
+            }
+        });
 
     public HomeFragment() {
         // Required empty public constructor
@@ -36,6 +62,34 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = HomeFragmentBinding.inflate(inflater);
 
+        todoApp = ((TodoApp) this.requireActivity().getApplication());
+
+        binding.secondsNot.setText("2");
+        binding.buttonNot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(
+                    HomeFragment.this.requireActivity(), android.Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    // You can use the API that requires the permission.
+                    programNotification();
+                } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    HomeFragment.this.requireActivity(), android.Manifest.permission.POST_NOTIFICATIONS)) {
+                    // In an educational UI, explain to the user why your app requires this
+                    // permission for a specific feature to behave as expected, and what
+                    // features are disabled if it's declined. In this UI, include a
+                    // "cancel" or "no thanks" button that lets the user continue
+                    // using your app without granting the permission.
+                    Toast.makeText(HomeFragment.this.requireActivity(), "You need permissions to send notifications!!!!.",
+                        Toast.LENGTH_LONG).show();
+                } else {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissionLauncher.launch(
+                        android.Manifest.permission.POST_NOTIFICATIONS);
+                }
+            }
+        });
 
         return binding.getRoot();
     }
@@ -45,7 +99,7 @@ public class HomeFragment extends Fragment {
         super.onStart();
 
         ArrayAdapter<CharSequence> adapter_xml = ArrayAdapter.createFromResource(
-            getActivity(),
+            requireActivity(),
             R.array.planets_array,
             android.R.layout.simple_spinner_item
         );
@@ -56,7 +110,7 @@ public class HomeFragment extends Fragment {
         spDefault.setAdapter(adapter_xml);
 
         ArrayAdapter<CharSequence> adapter_runtime = new ArrayAdapter<>(
-            getActivity(),
+            requireActivity(),
             android.R.layout.simple_spinner_item
         );
         adapter_runtime.add("First option");
@@ -68,9 +122,24 @@ public class HomeFragment extends Fragment {
         adapter_runtime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
         spRuntime.setAdapter(adapter_runtime);
+    }
 
+    private void programNotification() {
+        Intent notificationIntent = new Intent(this.requireActivity(), ShowNotification.class);
 
+        PendingIntent contentIntent = PendingIntent.getService(this.requireActivity(), 0,
+                notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
-
+        AlarmManager am = (AlarmManager) this.requireActivity().getSystemService(Context.ALARM_SERVICE);
+        // am.cancel(contentIntent);
+        if (am.canScheduleExactAlarms()) {
+            long secs = Long.parseLong(binding.secondsNot.getText().toString());
+            var triggerAtMillis = System.currentTimeMillis() + secs * 1000;
+            am.setExact(AlarmManager.RTC_WAKEUP, triggerAtMillis, contentIntent);
+        } else {
+            var i = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            this.requireActivity().startActivity(i);
+        }
     }
 }
